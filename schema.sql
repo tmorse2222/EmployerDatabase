15c779af7ac5548a7365e74154fc5c63fd228d51
Tyler M <tmorse2222@gmail.com>
2022-09-07 16:58:52 -0700

    Created Database; Created tables with columns for employee, role, and department

---
+++ schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE employee (
+id INT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+roleID INT NOT NULL,
+managerID INT,
+PRIMARY KEY (roleID)
+);
+
+CREATE TABLE role (
+id INT PRIMARY KEY,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(10,2) NOT NULL,
+departmentID INT NOT NULL,
+PRIMARY KEY (departmentID),
+FOREIGN KEY (id)
+REFERENCES employee(roleID)
+ON DELETE SET NULL
+);
+
+CREATE TABLE department (
+id INT PRIMARY KEY,
+name VARCHAR(30) NOT NULL,
+FOREIGN KEY (id)
+REFERENCES role(departmentID)
+ON DELETE SET NULL
+);
