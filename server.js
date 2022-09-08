3e7bfb08081971185ac1080246e719bae21d92c6
Tyler M <tmorse2222@gmail.com>
2022-09-07 17:24:20 -0700

    Created start() function w/ prompt for next steps

--- server.js
+++ server.js
@@ -12,3 +12,48 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employee_db database.`)
 );
+
+function start() {
+    inquirer.prompt({
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'action',
+        choices: [
+            'View all employees',
+            'Add employee',
+            'Update employee role',
+            'View all roles',
+            'Add role',
+            'View all departments',
+            'Add department',
+            'Quit'
+        ]
+    }).then((answer) => {
+        switch (answer.action) {
+            case 'View all employees':
+                viewAllEmployees();
+                break;
+            case 'Add employee':
+                addEmployee();
+                break;
+            case 'Update employee role':
+                updateEmployeeRole();
+                break;
+            case 'View all roles':
+                viewAllRoles();
+                break;
+            case 'Add role':
+                addRole();
+                break;
+            case 'View all departments':
+                viewAllDepartments();
+                break;
+            case 'Add department':
+                addDepartment();
+                break;
+            case 'Quit':
+                quit();
+                break;
+        }
+    });
+};
